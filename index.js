07252554d141ee92ba0a7ae8888225c9a6958c7c
DeepakAvadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2025-03-19 14:35:38 +0530

    updated tourpackage controller, mainfile, migration and the schema

--- index.js
+++ index.js
@@ -23,3 +23,7 @@ app.use("/api/gallery", gallery);
 app.use("/api/review/", reviewRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
