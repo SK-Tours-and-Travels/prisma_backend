22d499be8c04f20c2557341622b62febd5d7c06b
DeepakAvadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2025-04-25 16:01:20 +0530

    added localhost for cors

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(
       "https://jolly-smoke-0f8c68a00.6.azurestaticapps.net",
       "https://trippcard.in",
       "http://127.0.0.1:5173",
+      "http://localhost:5173",
       "http://127.0.0.1:3000",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
